3ab2ded2f48c4401e5467425e77ecfb8faa69181
Nik <nikgajsek31@gmail.com>
2022-09-19 18:06:12 +0200

    Edited header

--- header.php
+++ header.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
 <html>
-<head></head>
+<head><link rel="preconnect" href="https://fonts.googleapis.com/%22%3E
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <meta charset="utf-8">
+    <title>MYSTBOX</title>
+    <link rel="stylesheet" href="style.css"></head>
 <body>
