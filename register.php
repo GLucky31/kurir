d1ab9579a88392014f0acb55a5ec5cf2f6ea801d
Nik <nikgajsek31@gmail.com>
2022-10-16 17:00:04 +0200

    yessir

--- register.php
+++ register.php
@@ -12,13 +12,14 @@
         <div class="Container">
             <div class="register">
                 <h1>CREATE AN ACCOUNT</h1>
-                <form action="insert.php" method="post">
-                    <input class="input" type="text" name="Name" placeholder="Name" required="required">
-                    <input class="input" type="text" name="Surname" placeholder="Surname" required="required">
-                    <input class="input" type="text" name="Email" placeholder="E-mail" required="required">
+                <form action="user_insert.php" method="post">
+                    <input class="input" type="text" name="username" placeholder="Name" required="required">
+                    <input class="input" type="text" name="email" placeholder="E-mail" required="required">
                     <input class="input" type="text" name="Phone" placeholder="Phone" required="required">
                     <input class="input" type="password" name="password" placeholder="Password" required="required">
                     <input class="input" type="password" name="password2" placeholder="Confirm Password" required="required">
+                    <input class="input" type="radio" name="type[]" value="1">user
+                    <input class="input" type="radio" name="type[]" value="2">kurir
                     <p>Registered user?</p><a href="login.php">Log in</a>
                     <div>
                     <button type="submit" class="button">Register</button>
--- user_insert.php
+++ user_insert.php
@@ -1,32 +1,64 @@
 <?php
 include_once './database.php';
+if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['Phone']) && isset($_POST['password']) && isset($_POST['password2']) && isset($_POST['type'])){
+
 
 $username = $_POST['username'];
+$phone = $_POST['phone'];
 $email = $_POST['email'];
-$pass1 = $_POST['pass1'];
-$pass2 = $_POST['pass2'];
-//preverim podatke, da so obvezi vnešeni
-if (!empty($username) 
-        && !empty($email)  && !empty($pass1)
-        && ($pass1 == $pass2)) {
-    
-         $check="SELECT * FROM users WHERE email=?"
-         $stmt = $pdo->prepare($query);
-         $stmt->execute([$email]);
-         $count = $stmt->rowCount();
-         if(!($count>0)){
-    //$pass = sha1($pass1.$salt);
-    $pass = password_hash($pass1, PASSWORD_DEFAULT);
-    
-    $query = "INSERT INTO users (username,email,"
-            . "pass) "
-            . "VALUES (?,?,?)";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$username,$email,$pass]);
-    
-    header("Location: login.php");}
+$pass1 = $_POST['password1'];
+$pass2 = $_POST['password2'];
+$type = $_POST['type'];
+if($type==1){
+    $query="SELECT * FROM users WHERE email=?";
+    $stmt=$pdo->prepare($query);
+    $stmt->execute([$email]);
+    $countU = $stmt->rowCount();
+    $query ="SELECT * FROM kurir WHERE email=?";
+    $stmt=$pdo->prepare($query);
+    $stmt->execute([$email]);
+    $countK = $stmt->rowCount();
+    if($countU==0 && $countK==0){
+        if($pass1==$pass2){
+            $query = "INSERT INTO users (username, phone, email, password) VALUES (?,?,?,?)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$username, $phone, $email, $pass1]);
+            header("Location: login.php");
+        }
+        else{
+            echo "Passwords don't match";
+        }
+    }
+    else{
+        echo "Email already exists";
+    }
 }
-else {
-    header("Location: registration.php");
+else{
+    $query="SELECT * FROM users WHERE email=?";
+    $stmt=$pdo->prepare($query);
+    $stmt->execute([$email]);
+    $countU = $stmt->rowCount();
+    $query ="SELECT * FROM kurir WHERE email=?";
+    $stmt=$pdo->prepare($query);
+    $stmt->execute([$email]);
+    $countK = $stmt->rowCount();
+    if($countU==0 && $countK==0){
+        if($pass1==$pass2){
+            $query = "INSERT INTO kurir (username, phone, email, password) VALUES (?,?,?,?)";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$username, $phone, $email, $pass1]);
+            header("Location: login.php");
+        }
+        else{
+            echo "Passwords don't match";
+        }
+    }
+    else{
+        echo "Email already exists";
+    }
+}
+}
+else{
+    echo "Please fill all fields";
 }
 ?>
